55447d2137e879056d09f18ad9dee62d6fdfb555
Sergio Souza <serjsouza93@gmail.com>
2022-08-24 20:27:15 -0300

    finalizando operacoes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function insertNumber(num){
-  var number = document.getElementById('resultado').innerHTML
+  var number = document.getElementById('resultado').innerHTML;
   document.getElementById('resultado').innerHTML = number + num;
 }
 
 function clearNumber(){
   document.getElementById('resultado').innerHTML = "";
+}
+
+function backspace() {
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length-1)
+}
+
+function calculate(){
+  var resultado = document.getElementById('resultado').innerHTML;
+  resultado ? document.getElementById('resultado').innerHTML = eval(resultado) : false
 }
